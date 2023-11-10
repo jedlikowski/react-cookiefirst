df1ba4b235223f17186461d89e2165cfca187afe
Juan Jose Carracedo <34981526+DonsWayo@users.noreply.github.com>
2023-11-10 15:12:02 +0100

    Add type module to package.json and assert json type in rollup.config.js import of package.json. Ignore deprecations in tsconfig.json.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
-import pkg from "./package.json";
+import pkg from "./package.json" assert { type: "json" }; 
 import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 
